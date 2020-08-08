6b91e66b16a98650b886ad78d458169cbe4af935
Rumen G.Bogdanovski <rumen@skyarchive.org>
2020-08-08 19:23:36 +0300

    remove second attempt to load imager agent

--- handlepropertychange.cpp
+++ handlepropertychange.cpp
@@ -534,13 +534,17 @@ void ImagerWindow::property_define(indigo_property* property, char *message) {
 	char selected_agent[INDIGO_VALUE_SIZE];
 
 	if (!strncmp(property->device, "Server", 6)) {
+		static bool load = true;
 		if (client_match_device_property(property, property->device, "DRIVERS")) {
-			if (!indigo_get_switch(property, "indigo_agent_imager")) {
+			if (!indigo_get_switch(property, "indigo_agent_imager") && load) {
 				QtConcurrent::run([=]() {
 					static const char *items[] = { "DRIVER" };
 					static const char *values[] = { "indigo_agent_imager" };
 					indigo_change_text_property(NULL, property->device, "LOAD", 1, items, values);
 				});
+				load = false;
+			} else {
+				load = true;
 			}
 		}
 		return;
