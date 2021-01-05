0be3804490bbc3316300dea64e901e6c004eb421
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-01-05 22:52:12 +0200

    build_windows.sh builds 64 and 32 bit exes

--- windows/build_windows.sh
+++ windows/build_windows.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-export PATH=$PATH:/c/Qt/5.12.10/mingw73_64/bin:/c/Qt/Tools/mingw730_64/bin/
 
-qmake ../ain_imager.pro
+SAVE_PATH=$PATH
+
+export PATH=/c/Qt/5.12.10/mingw73_32/bin:/c/Qt/Tools/mingw730_32/bin/:$SAVE_PATH
+mkdir ain_32
+cd ain_32
+qmake ../../ain_imager.pro
+mingw32-make
+
+cd ..
+
+export PATH=/c/Qt/5.12.10/mingw73_64/bin:/c/Qt/Tools/mingw730_64/bin/:$SAVE_PATH
+mkdir ain_64
+cd ain_64
+qmake ../../ain_imager.pro
 mingw32-make
