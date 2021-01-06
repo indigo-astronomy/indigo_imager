2814f7b0857b849ee7368f551f11bace2d3a6b5b
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-01-06 16:27:20 +0200

    windows build automated

--- ain_imager.pro
+++ ain_imager.pro
@@ -1,5 +1,5 @@
 QT += core gui widgets network printsupport concurrent
-CONFIG += c++11 debug
+CONFIG += c++11 release
 
 OBJECTS_DIR=object
 MOC_DIR=moc
--- windows/build_windows.sh
+++ windows/build_windows.sh
@@ -3,15 +3,27 @@
 SAVE_PATH=$PATH
 
 export PATH=/c/Qt/5.12.10/mingw73_32/bin:/c/Qt/Tools/mingw730_32/bin/:$SAVE_PATH
-mkdir ain_32
+
+pushd .
+[ ! -d "ain_32" ] && mkdir ain_32
 cd ain_32
 qmake ../../ain_imager.pro
-mingw32-make
+mingw32-make -f Makefile.release
 
-cd ..
+[ ! -d "ain_imager" ] && mkdir ain_imager
+cd ain_imager
+cp ../release/ain_imager.exe .
+windeployqt ain_imager.exe
 
+popd
 export PATH=/c/Qt/5.12.10/mingw73_64/bin:/c/Qt/Tools/mingw730_64/bin/:$SAVE_PATH
-mkdir ain_64
+[ ! -d "ain_64" ] && mkdir ain_64
 cd ain_64
 qmake ../../ain_imager.pro
-mingw32-make
+mingw32-make -f Makefile.release
+
+[ ! -d "ain_imager" ] && mkdir ain_imager
+cd ain_imager
+cp ../release/ain_imager.exe .
+windeployqt ain_imager.exe
+
