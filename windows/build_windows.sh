30c4ddbf3c46993c3bd0aa4b1faef1b349ee55a5
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-01-06 17:35:19 +0200

    fully automate windows installer build

--- windows/build_windows.sh
+++ windows/build_windows.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-SAVE_PATH=$PATH
+APP=ain_imager
+
+SAVE_PATH=$PATH:/c/Program\ Files\ \(x86\)/Inno\ Setup\ 6/
 
 export PATH=/c/Qt/5.12.10/mingw73_32/bin:/c/Qt/Tools/mingw730_32/bin/:$SAVE_PATH
 
 pushd .
-[ ! -d "ain_32" ] && mkdir ain_32
-cd ain_32
-qmake ../../ain_imager.pro
+[ ! -d "${APP}_32" ] && mkdir ${APP}_32
+cd ${APP}_32
+qmake ../../${APP}.pro
 mingw32-make -f Makefile.release
 
-[ ! -d "ain_imager" ] && mkdir ain_imager
-cd ain_imager
-cp ../release/ain_imager.exe .
-windeployqt ain_imager.exe
-
+[ ! -d "${APP}" ] && mkdir ${APP}
+cd ${APP}
+cp ../release/${APP}.exe .
+windeployqt ${APP}.exe
 popd
+
+pushd .
 export PATH=/c/Qt/5.12.10/mingw73_64/bin:/c/Qt/Tools/mingw730_64/bin/:$SAVE_PATH
-[ ! -d "ain_64" ] && mkdir ain_64
-cd ain_64
-qmake ../../ain_imager.pro
+[ ! -d "${APP}_64" ] && mkdir ${APP}_64
+cd ${APP}_64
+qmake ../../${APP}.pro
 mingw32-make -f Makefile.release
 
-[ ! -d "ain_imager" ] && mkdir ain_imager
-cd ain_imager
-cp ../release/ain_imager.exe .
-windeployqt ain_imager.exe
+[ ! -d "${APP}" ] && mkdir ${APP}
+cd ${APP}
+cp ../release/${APP}.exe .
+windeployqt ${APP}.exe
+popd
 
+iscc -DArch=32 ${APP}.iss
+iscc -DArch=64 ${APP}.iss
