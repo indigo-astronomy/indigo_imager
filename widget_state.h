c4f6a43168900b6b599502a7e88748844cc205a6
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-01-26 10:24:01 +0200

    fix some colours

--- widget_state.h
+++ widget_state.h
@@ -20,9 +20,9 @@
 #define _WIDGET_STATE_H
 
 #define set_alert(widget) (widget->setStyleSheet("*:enabled { background-color: #312222;} *:!enabled { background-color: #292222;}"))
-#define set_idle(widget) (widget->setStyleSheet("*:enabled {background-color: #272727;} *:!enabled {background-color: #272727;}"))
+#define set_idle(widget) (widget->setStyleSheet("*:enabled {background-color: #252525;} *:!enabled {background-color: #252525;}"))
 #define set_busy(widget) (widget->setStyleSheet("*:enabled {background-color: #313120;} *:!enabled {background-color: #292920;}"))
-#define set_ok(widget) (widget->setStyleSheet("*:enabled {background-color: #272727;} QSpinBox:!enabled {background-color: #202020;}"))
+#define set_ok(widget) (widget->setStyleSheet("*:enabled {background-color: #252525;} QSpinBox:!enabled {background-color: #202020;}"))
 #define set_ok2(widget) (widget->setStyleSheet("background-color: #273727;"))
 
 #endif /* _WIDGET_STATE_H */
