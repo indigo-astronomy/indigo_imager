264d3d447ac3ae4beeff41272f4183c98311288d
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-08-10 21:39:44 +0300

    typo fix

--- ain_imager_src/solver_tab.cpp
+++ ain_imager_src/solver_tab.cpp
@@ -245,8 +245,8 @@ void ImagerWindow::create_solver_tab(QFrame *solver_frame) {
 void ImagerWindow::update_solver_widgets_at_start(const char *imager_agent, const char *solver_agent) {
 	bool done = false;
 	int wait_busy = 25;
-	set_text(m_solver_status_label1, "<img src=\":resource/led-orange.png\"> Witing for image");
-	set_text(m_solver_status_label2, "<img src=\":resource/led-orange.png\"> Witing for image");
+	set_text(m_solver_status_label1, "<img src=\":resource/led-orange.png\"> Waiting for image");
+	set_text(m_solver_status_label2, "<img src=\":resource/led-orange.png\"> Waiting for image");
 	set_widget_state(m_mount_solve_and_sync_button, INDIGO_BUSY_STATE);
 	set_widget_state(m_mount_solve_and_center_button, INDIGO_BUSY_STATE);
 	set_widget_state(m_solve_button, INDIGO_BUSY_STATE);
