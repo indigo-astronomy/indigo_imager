8ad55f7c1a3b736776920f52f7e7cb79cf3488c0
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-09-05 03:44:46 +0300

    use blob_preview_cache::stretch() method intead of recreate in stretching - creashes seem to be fixed

--- ain_imager_src/imagerwindow.cpp
+++ ain_imager_src/imagerwindow.cpp
@@ -783,7 +783,7 @@ void ImagerWindow::on_use_system_locale_changed(bool status) {
 void ImagerWindow::on_imager_stretch_changed(int level) {
 	conf.preview_stretch_level = (preview_stretch)level;
 	const stretch_config_t sc = {conf.preview_stretch_level, conf.preview_color_balance};
-	preview_cache.recreate(m_image_key, m_indigo_item, sc);
+	preview_cache.stretch(m_image_key, sc);
 	show_preview_in_imager_viewer(m_image_key);
 	write_conf();
 	indigo_debug("%s\n", __FUNCTION__);
@@ -792,7 +792,7 @@ void ImagerWindow::on_imager_stretch_changed(int level) {
 void ImagerWindow::on_imager_cb_changed(int balance) {
 	conf.preview_color_balance = (color_balance)balance;
 	const stretch_config_t sc = {conf.preview_stretch_level, conf.preview_color_balance};
-	preview_cache.recreate(m_image_key, m_indigo_item, sc);
+	preview_cache.stretch(m_image_key, sc);
 	show_preview_in_imager_viewer(m_image_key);
 	write_conf();
 	indigo_debug("%s\n", __FUNCTION__);
