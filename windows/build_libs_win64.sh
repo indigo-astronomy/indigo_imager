e6244e6449d711d09cdd40f9f3698acf63450837
Rumen G.Bogdanovski <rumen@skyarchive.org>
2022-04-05 14:33:22 +0300

    fix build_libs_win*.sh

--- windows/build_libs_win64.sh
+++ windows/build_libs_win64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PATH=/c/Qt/Tools/mingw730_64/bin/:$PATH
-cd ../external/LibRaw-0.20.2
+cd ../external/libraw
 mingw32-make -f Makefile.mingw clean
 mkdir lib/
 mingw32-make -f Makefile.mingw library
