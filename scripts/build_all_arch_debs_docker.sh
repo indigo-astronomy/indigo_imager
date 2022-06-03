8c2bcb6724b4f9285fc0ad6e0709ab4a9efd03bb
Rumen G.Bogdanovski <rumen@skyarchive.org>
2022-06-03 22:20:06 +0300

    fix docker build

--- scripts/build_all_arch_debs_docker.sh
+++ scripts/build_all_arch_debs_docker.sh
@@ -12,6 +12,6 @@ sh scripts/make_source_tarball.sh $VERSION
 sh scripts/build_in_docker.sh "i386/debian:stretch-slim" $VERSION "i386"
 sh scripts/build_in_docker.sh "amd64/debian:stretch-slim" $VERSION "amd64"
 sh scripts/build_in_docker.sh "arm32v7/debian:buster-slim" $VERSION "armhf"
-sh scripts/build_in_docker.sh "arm64v8/debian:stretch-slim" $VERSION "arm64"
+sh scripts/build_in_docker.sh "arm64v8/debian:buster-slim" $VERSION "arm64"
 rm ain-imager-$VERSION.tar.gz
 
