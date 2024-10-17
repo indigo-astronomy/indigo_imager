91d6d6508d266a162930280ed8a61e8b77bf83b8
Rumen G.Bogdanovski <rumenastro@gmail.com>
2024-10-17 16:19:38 +0300

    fix selection restoration after fosuing/guiding with selection

--- ain_imager_src/imagerwindow.cpp
+++ ain_imager_src/imagerwindow.cpp
@@ -757,6 +757,8 @@ void ImagerWindow::on_create_preview(indigo_property *property, indigo_item *ite
 			indigo_debug("m_imager_viewer = %p", m_imager_viewer);
 			m_imager_viewer->setText(QString("Unsaved") + QString(m_indigo_item->blob.format));
 			m_imager_viewer->setToolTip(QString("Unsaved") + QString(m_indigo_item->blob.format));
+			int size = (int)round(m_focus_star_radius->value() * 2 + 1);
+			move_resize_focuser_selection(m_star_x->value(), m_star_y->value(), size);
 		}
 		indigo_debug("save_blob: %d", save_blob);
 		if (save_blob && strcasecmp(".raw", m_indigo_item->blob.format)) {
@@ -846,6 +848,8 @@ void ImagerWindow::on_create_preview(indigo_property *property, indigo_item *ite
 			if (show_preview_in_guider_viewer(key)) {
 				indigo_debug("m_guider_viewer = %p", m_guider_viewer);
 				//m_guider_viewer->setText(QString("Guider: image") + QString(item->blob.format));
+				int size = (int)round(m_guide_star_radius->value() * 2 + 1);
+				move_resize_guider_selection(m_guide_star_x->value(), m_guide_star_y->value(), size);
 			}
 		} else {
 			preview_cache.remove(property, item);
