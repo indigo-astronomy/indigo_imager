8725f24c15aa3c78e83aa8cfc0a5b058603faedb
Rumen G.Bogdanovski <rumenastro@gmail.com>
2025-07-15 01:53:41 +0300

    make ain require version 336

--- ain_imager_src/handlepropertychange.cpp
+++ ain_imager_src/handlepropertychange.cpp
@@ -2612,8 +2612,8 @@ void ImagerWindow::property_define(indigo_property* property, char *message) {
 				int build;
 				char message[255];
 				sscanf(item->text.value, "%d.%d-%d", &version_major, &version_minor, &build);
-				if (build < 328 && !properties.get(property->device, SERVER_INFO_PROPERTY_NAME)) { /* show warning only once per connection */
-					sprintf(message, "WARNING: Some features will not work on '%s' running Indigo %s as Ain requires 2.0-328 or newer!", property->device, item->text.value);
+				if (build < 336 && !properties.get(property->device, SERVER_INFO_PROPERTY_NAME)) { /* show warning only once per connection */
+					sprintf(message, "WARNING: Some features will not work on '%s' running Indigo %s as Ain requires 2.0-336 or newer!", property->device, item->text.value);
 					window_log(message, INDIGO_BUSY_STATE);
 				}
 			}
